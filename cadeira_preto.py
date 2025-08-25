7de4a446e63147239455f2e11e66b6a70635f1c7
Rafael Sampaio <160675357+rafaeljuniora@users.noreply.github.com>
2025-08-25 20:35:25 -0300

    Update cadeira_preto.py

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,1 +1,3 @@
-
+# oque é um cabeçalho dentro de um arquivo python e pra que serve oque é um docstring e pra que serve
+# é o comentario inicial do arquivo utilizado para informar o proposito e funcionalidade do arquivo
+# docstring no python são strings usadas para documentar módulos, classes, funções e métodos, utiliza """ pra demarcar e o __doc__ para acessar o comentario
