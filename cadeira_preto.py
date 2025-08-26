db5696dc4f02e1fa9c178232d623315accde653b
Fabiano Dicheti <117326271+FabianoDicheti@users.noreply.github.com>
2025-08-26 15:50:58 -0300

    Update cadeira_preto.py (#1)

--- cadeira_preto.py
+++ cadeira_preto.py
@@ -1,3 +1,34 @@
 # oque é um cabeçalho dentro de um arquivo python e pra que serve oque é um docstring e pra que serve
 # é o comentario inicial do arquivo utilizado para informar o proposito e funcionalidade do arquivo
 # docstring no python são strings usadas para documentar módulos, classes, funções e métodos, utiliza """ pra demarcar e o __doc__ para acessar o comentario
+
+
+
+
+######################################################################
+
+# formate o cabeçalho deste arquivo, depois implemente as funções abaixo e coloque as docstrings
+
+def maximo(nums):
+    """oque faz
+    oque recebe
+    oque retorna"""
+    # TODO: percorra a lista guardando o maior atual
+    ...
+
+import re
+
+def limpa_texto(s: str) -> str:
+    """..."""
+    # TODO: converta s para minúsculo e remova pontuações como ,.;:!?'"()-_
+    ...
+
+def melhor_vendedor(totais: dict):
+    """
+    ...
+    Recebe ...
+    Retorna (nome, total) com o maior total.
+    Se dict vazio, levante ValueError.
+    """
+    # TODO: encontre o par com maior total (sem ordenar a lista inteira)
+    ...
